f988211a2038843ad3a92a0d53b8458629a8edf6
everyoneatch <everyoneatch@gmail.com>
2022-05-01 23:21:48 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+alert('Selamat Hari Raya Idul Fitri 1443H');
+alert('Kami segenap keluarga sapto family ingin mengucapkan');
+alert('Minal Aidin Wal Faizin. Mohon Maaf Lahir Dan Batin');
+alert('Semoga Kita Dipertemukan Pada Bulan Ramadhan Berikutnya Dan Sealu Diberi Kesehatan');
+alert('AMIN!!! Ya Rabalalamin');
